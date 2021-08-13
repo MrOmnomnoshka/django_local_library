9bdaf1c44dd60d23a05324ff64a997663c1c308f
MrOmnomnoshka <MrOmnomnoshka@gmail.com>
2021-08-14 01:56:47 +0300

    First version of application moved into github

---
+++ catalog/urls.py
@@ -0,0 +1,47 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('books/', views.BookListView.as_view(), name='books'),
+    path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
+    path('authors/', views.AuthorListView.as_view(), name='authors'),
+    path('author/<int:pk>',
+         views.AuthorDetailView.as_view(), name='author-detail'),
+]
+
+
+urlpatterns += [
+    path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
+    path('borrowed/', views.LoanedBooksAllListView.as_view(), name='all-borrowed'),
+]
+
+
+# Add URLConf for librarian to renew a book.
+urlpatterns += [
+    path('book/<uuid:pk>/renew/', views.renew_book_librarian, name='renew-book-librarian'),
+    # path('book/<uuid:pk>/loan/', views.renew_book_librarian('GET'), name='loan-book-librarian'),
+]
+
+
+# Add URLConf to create, update, and delete authors
+urlpatterns += [
+    path('author/create/', views.AuthorCreate.as_view(), name='author-create'),
+    path('author/<int:pk>/update/', views.AuthorUpdate.as_view(), name='author-update'),
+    path('author/<int:pk>/delete/', views.AuthorDelete.as_view(), name='author-delete'),
+]
+
+# Add URLConf to create, update, and delete books
+urlpatterns += [
+    path('book/create/', views.BookCreate.as_view(), name='book-create'),
+    path('book/<int:pk>/update/', views.BookUpdate.as_view(), name='book-update'),
+    path('book/<int:pk>/delete/', views.BookDelete.as_view(), name='book-delete'),
+]
+
+# Add register from
+"""https://pocoz.gitbooks.io/django-v-primerah/content/glava-4-sozdanie-social-website/
+registratsiya-polzovatelei-i-profili-polzovatelei/registratsiya-polzovatelei.html"""
+urlpatterns += [
+    path('register', views.register, name='register'),
+]
